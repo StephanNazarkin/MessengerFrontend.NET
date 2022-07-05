11ac2dbad25b71956b08c99a04143bb49b2ef4e2
StephanNazarkin <stepan.nazarkin@ukr.net>
2022-07-05 19:50:56 +0300

    settings page has been created

--- MessengerFrontend/Program.cs
+++ MessengerFrontend/Program.cs
@@ -24,4 +24,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(name: "userProfile", pattern: "{controller=User}/{action=Profile}/{id?}");
+
 app.Run();
