9db192c563787f80847c102ca0312ddad63da2e4
DefinitelyLD <67641038+DefinitelyLD@users.noreply.github.com>
2022-07-05 16:04:31 +0300

    Sign in/up + confirm email pages implemented

---
+++ MessengerFrontend/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MessengerFrontend.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult ConfirmEmail()
+        {
+            return View();
+        }
+    }
+}
--- MessengerFrontend/Controllers/HomeController.cs
+++ MessengerFrontend/Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace MessengerFrontend.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
