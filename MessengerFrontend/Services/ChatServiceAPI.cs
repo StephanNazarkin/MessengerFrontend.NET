2cb90e3646ee7afdccb428a13f81020f67c79fec
vladimir-riabenko <76427828+vladimir-riabenko@users.noreply.github.com>
2022-07-10 15:47:20 +0300

    dynamic page load without refresh

---
+++ MessengerFrontend/Services/ChatServiceAPI.cs
@@ -0,0 +1,27 @@
+﻿using MessengerFrontend.Models.Chat;
+using MessengerFrontend.Services.Interfaces;
+using System.Text.Json;
+
+namespace MessengerFrontend.Services
+{
+    public class ChatServiceAPI : IChatServiceAPI
+    {
+        private readonly HttpClient _httpClient;
+
+        public ChatServiceAPI(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("Messenger");
+        }
+
+        public async Task<IEnumerable<ChatViewModel>> GetAllChatrooms()
+        {
+            var httpResponseMessage = await _httpClient.GetAsync("Chatroom/GetAllChatrooms");
+            using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
+
+            var chats = await JsonSerializer.DeserializeAsync
+                <IEnumerable<ChatViewModel>>(contentStream);
+
+            return chats;
+        }
+    }
+}
