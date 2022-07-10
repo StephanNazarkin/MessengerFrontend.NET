e1fec4e6787e939da907068456cbff1335b07268
StephanNazarkin <stepan.nazarkin@ukr.net>
2022-07-10 15:54:36 +0300

    Test Commit

---
+++ MessengerFrontend/Models/Users/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MessengerFrontend.Models.Users
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
